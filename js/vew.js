e60a93a508604ed48ee8e1edb681a08d611e38cc
an.tran <an.tran@globee.hk>
2020-12-26 12:02:33 +0700

    [master]: update

--- js/vew.js
+++ js/vew.js
@@ -10,11 +10,19 @@ $( document ).ready(function() {
 
     $("tr").on("click",function() {
         $('#content-table').removeClass("d-lg-block");
-        $('#detail-section').addClass("d-block").removeClass("d-none");
+        $('#content-table-mobile').removeClass("d-block").addClass("d-none");
+        $('#detail-section').addClass("d-lg-block").removeClass("d-none");
     });
 
     $(".back-btn").on("click",function() {
         $('#detail-section').addClass("d-none").removeClass("d-block");
         $('#content-table').addClass("d-lg-block");
-    })
+        $('#content-table-mobile').removeClass("d-none").addClass("d-block");
+    });
+
+    $("#add-condition").on("click",function() {
+        var templateStr = '<tr><td class="width-51"><div class="d-flex align-items-center"><div class="mr-2 d-none d-md-block"><b>Từ</b></div><input class="form-control col-sm-7" type="text" required></div></td><td class="width-51"><div class="d-flex align-items-center"><input class="form-control col-sm-7" type="text" required><div class="mx-2 currency-ico d-none d-md-block"><b>VND</b></div><div class="bg-secondary percent-ico"><b>%</b></div></div></td><td><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></td></tr>';
+        $("#type-info table tbody").append(templateStr);
+        $("#type-promotion table tbody").append(templateStr);
+    });
 })
